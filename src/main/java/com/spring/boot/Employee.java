23c3f68dcc140ed56dd7f5630bc83d0ba8cc22f6
raj101aseel <raj101aseel@gmail.com>
2025-11-24 08:30:13 +0530

    Spring Boot Project

---
+++ src/main/java/com/spring/boot/Employee.java
@@ -0,0 +1,18 @@
+package com.spring.boot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Employee {
+	private int empId;
+	private String empName;
+	private double empSalary;
+}
